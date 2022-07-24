84d91f34a7b2c67079c05e8dd9a6bef0a0b02c1a
Arash Ramzani <arash1.ramzani@gmail.com>
2022-07-24 17:21:08 +0430

    choose random point to patrol

--- Assets/Src/Scripts/Enemy.cs
+++ Assets/Src/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,29 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
-    [SerializeField] private Transform _target;
+    private Transform _target;
+
+    [SerializeField] private EnemyPatrol enemyPatrol;
 
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(SetNewPatrolPoint());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _navMeshAgent.SetDestination(_target.position);
+        Debug.Log(_navMeshAgent.remainingDistance);
+        if (_navMeshAgent.remainingDistance < 1f)
+        {
+            StartCoroutine(SetNewPatrolPoint());
+        }
+    }
+
+    private IEnumerator SetNewPatrolPoint()
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0.5f, 3f));
+        _navMeshAgent.SetDestination(enemyPatrol.RandomPatrolPoint);
     }
 }
