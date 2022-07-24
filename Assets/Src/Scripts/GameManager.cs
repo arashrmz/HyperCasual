128a46725f0fa38745f37b0295772d16a85f02ee
Arash Ramzani <arash1.ramzani@gmail.com>
2022-07-24 18:24:40 +0430

    add basic game functionality to GameManager

--- Assets/Src/Scripts/GameManager.cs
+++ Assets/Src/Scripts/GameManager.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private int _keysCollected = 0;
+    private bool _isDoorOpen;
 
+    [SerializeField] private int keysToCollect = 5;
+
+    public int TotalKeys { get => keysToCollect; }
+
+    private void Start()
+    {
+        UIManager.Instance.UpdateKeyText(_keysCollected);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerDiscovered()
     {
+        Debug.Log("Player discovered");
+    }
 
+    public void OnKeyCollected()
+    {
+        _keysCollected++;
+        UIManager.Instance.UpdateKeyText(_keysCollected);
+        if (_keysCollected == keysToCollect)
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        _isDoorOpen = true;
+        Debug.Log("Door opened");
+    }
+
+    public void OnEnteredDoor()
+    {
+        if (_isDoorOpen)
+        {
+            Debug.Log("You win!");
+            UIManager.Instance.Win();
+        }
+    }
+
+    public void OnDiscoveredByEnemy()
+    {
+        Debug.Log("You lose");
+        UIManager.Instance.GameOver();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
