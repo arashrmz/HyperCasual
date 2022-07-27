948a36cea3208f6d8c81d367deb2ae31f6254c51
Arash Ramzani <arash1.ramzani@gmail.com>
2022-07-27 16:31:56 +0430

    add player states

--- Assets/Src/Scripts/PlayerManager.cs
+++ Assets/Src/Scripts/PlayerManager.cs
@@ -1,19 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    public enum PlayerState
+    {
+        Idle,
+        Moving,
+        Start,
+        Crash,
+        Falling
+    }
+
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDistance = 1f;
+    [SerializeField] private PlayerAnimation playerAnimation;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private PlayerState _playerState = PlayerState.Idle;
+
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        playerAnimation = GetComponent<PlayerAnimation>();
+
+        _playerState = PlayerState.Idle;
+    }
+
+    public void StartGame()
+    {
+        _playerState = PlayerState.Start;
+        StartCoroutine(StartGameCoroutine());
+    }
+
+    private IEnumerator StartGameCoroutine()
+    {
+        playerAnimation.OnStart();
+        yield return new WaitForSeconds(1f);
+        _playerState = PlayerState.Moving;
+    }
 
     private void FixedUpdate()
     {
-        var grounded = Physics.Raycast(transform.position, -transform.up, groundDistance, groundLayer);
-        if (!grounded)
+        if (_playerState == PlayerState.Falling)
+            return;
+        RaycastHit hitInfo;
+        var grounded = Physics.Raycast(transform.position, -transform.up, out hitInfo, groundDistance, groundLayer);
+        if (!grounded && _playerState != PlayerState.Falling)
         {
-            Debug.Log("You lose");
+            _playerState = PlayerState.Falling;
             GameManager.Instance.OnFallDown();
+            playerAnimation.OnFalling();
+        }
+        else
+        {
+            if (hitInfo.collider.tag == "Dropping_Tile")
+            {
+                hitInfo.collider.GetComponent<DroppingTile>().Drop();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_playerState == PlayerState.Idle)
+        {
+            playerAnimation.OnIdle();
+        }
+        else if (_playerState == PlayerState.Moving)
+        {
+            //play move animation
+            playerAnimation.OnMoving();
+            //move the character
+            playerMovement.UpdateLogic();
+        }
+        else if (_playerState == PlayerState.Falling)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 5f);
         }
     }
 
@@ -25,7 +91,11 @@ public class PlayerManager : MonoBehaviour
         }
         else if (other.gameObject.tag == "Door")
         {
-            GameManager.Instance.OnEnteredDoor();
+            GameManager.Instance.OnEnteredDoor(other.GetComponentInParent<Door>());
+        }
+        else if (other.gameObject.tag == "Door_Range")
+        {
+            GameManager.Instance.OnEnteredDoorRange(other.GetComponent<Door>());
         }
     }
 
@@ -40,5 +110,10 @@ public class PlayerManager : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, -transform.up * groundDistance);
+    }
+
+    public void Crash()
+    {
+        throw new NotImplementedException();
     }
 }
