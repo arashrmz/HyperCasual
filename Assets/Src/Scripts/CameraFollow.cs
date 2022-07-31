642ba03304dd33405f6dce39cf5178f34c910179
Arash Ramzani <arash1.ramzani@gmail.com>
2022-08-01 00:35:15 +0430

    add event listener for ShouldFollow

--- Assets/Src/Scripts/CameraFollow.cs
+++ Assets/Src/Scripts/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,13 @@ public class CameraFollow : MonoBehaviour
     {
         ShouldFollow = true;
         offset = transform.position - target.transform.position;
+        GameManager.Instance.OnGameOver += GameOver;
+        GameManager.Instance.OnWinner += GameOver;
+    }
+
+    private void GameOver()
+    {
+        ShouldFollow = false;
     }
 
     void LateUpdate()
