8b1c5c5623a51af229233f31cf4f4b11a76c7da9
Arash Ramzani <arash1.ramzani@gmail.com>
2022-08-01 00:35:43 +0430

    change drop from physics to translate

--- Assets/Src/Scripts/DroppingTile.cs
+++ Assets/Src/Scripts/DroppingTile.cs
@@ -9,9 +9,17 @@ public class DroppingTile : MonoBehaviour
     [SerializeField] private float force = 1f;
 
     private Rigidbody _rigidbody;
+    private bool _isDropped = false;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isDropped)
+            transform.Translate(Vector3.down * force * Time.deltaTime);
     }
 
     public void Drop()
@@ -22,8 +30,7 @@ public class DroppingTile : MonoBehaviour
     private IEnumerator DropCoroutine()
     {
         yield return new WaitForSeconds(timeToDrop);
-        _rigidbody.useGravity = true;
-        _rigidbody.AddForce(Vector3.down * force, ForceMode.Impulse);
+        _isDropped = true;
         yield return new WaitForSeconds(4f);
         Destroy(gameObject);
     }
